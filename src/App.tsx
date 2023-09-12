4db69001ba356401ca54962ed794df40c257fee0
Ganea Andreea Maria <ganea.andreea.maria@gmail.com>
2023-09-12 18:23:10 +0300

    Created routes to payments and users Created payments JSON and populated it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Payments from "./components/Payments/Payments";
+import Home from "./components/Home/Home";
+import Users from "./components/Users/Users";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path='/payments' element={<Payments />} />
+        <Route path='/users' element={<Users />} />
       </Routes>
     </BrowserRouter>
   );
