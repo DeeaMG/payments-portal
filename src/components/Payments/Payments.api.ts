f30219a91c885c86e911adb4b350eb63b1b93c0a
Ganea Andreea Maria <ganea.andreea.maria@gmail.com>
2023-09-14 18:09:41 +0300

    Update API

--- src/components/Payments/Payments.api.ts
+++ src/components/Payments/Payments.api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001"; // Replace with your actual API base URL
+const API_BASE_URL = "http://localhost:3001";
 
 export const getPayments = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/payments`);
     return response.data;
   } catch (error: any) {
-    throw "Error fetching payments: " + error.message;
+    throw new Error("Error fetching payments: " + error.message);
   }
 };
